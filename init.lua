ed156d3502b82f3788daba4d3bab4d873ac11c36
puglia-ryan <ryan.puglia.001@student.uni.lu>
2024-06-26 22:06:50 +0200

    Changed the structure of the init file so that it adds every file from plugin_config individually

--- init.lua
+++ init.lua
@@ -1,7 +1,16 @@
 require("core.tabs_and_spacing")
 require("core.keymaps.language_keymaps")
 require("core.plugins")
-require("core.plugin_config")
+require("core.plugin_config.mason")
+require("core.plugin_config.autoclose")
+require("core.plugin_config.catppuccin")
+require("core.plugin_config.completions")
+require("core.plugin_config.gitsigns")
+require("core.plugin_config.lsp_config")
+require("core.plugin_config.lualine")
+require("core.plugin_config.nvim-tree")
+require("core.plugin_config.treesitter")
+
 
 --Allow line numbering 
 vim.o.number = true
