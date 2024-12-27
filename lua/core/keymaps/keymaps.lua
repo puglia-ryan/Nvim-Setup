31fa6b31b4dd1cae16b9c67b63f14cc36e8287cf
puglia-ryan <ryan.puglia.001@student.uni.lu>
2024-12-27 13:40:03 +0100

    Added a keymap to move back 1 tab lenght when pressing shift and tab

--- lua/core/keymaps/keymaps.lua
+++ lua/core/keymaps/keymaps.lua
@@ -3,3 +3,8 @@
 
 --Keymap to use Ctrl+f in order to use live_grep (search)
 vim.api.nvim_set_keymap('n', '<C-f>', ":lua require('telescope.builtin').live_grep()<CR>", { noremap = true, silent = true })
+
+--Shift+Tab to move back one tab length in normal and insert mode
+vim.keymap.set('i', '<S-Tab>', '<C-d>', { noremap = true, silent = true }) -- In insert mode
+vim.keymap.set('n', '<S-Tab>', '<<', { noremap = true, silent = true })    -- In normal mode
+
