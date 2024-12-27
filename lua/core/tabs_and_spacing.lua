4cfde5489b333428fb86537b89634670ffda5b7f
puglia-ryan <ryan.puglia.001@student.uni.lu>
2024-12-27 13:39:21 +0100

    Changed the settings for tabs and spacing along with a few other tweaks

--- lua/core/tabs_and_spacing.lua
+++ lua/core/tabs_and_spacing.lua
@@ -1,16 +1,18 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.opt.backspace = '2'
+vim.opt.backspace = {'indent', 'eol', 'start'}
 vim.opt.showcmd = true
-vim.opt.laststatus = 2
+vim.opt.laststatus = 3
 vim.opt.autowrite = true
 vim.opt.cursorline = true
+vim.opt.cursorcolumn = false
 vim.opt.autoread = true
+vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>', { silent = true })
 
 --use spaces for tabs and extra
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
